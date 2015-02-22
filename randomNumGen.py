3feaa712fb04d4f2bbcbe1e9f4150d6e2173ecc6
vagrant <vagrant@precise32.(none)>
2015-02-22 02:20:26 +0000

    first python script checking

---
+++ randomNumGen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import random
+
+"""
+Random number generator. You will have unlimited guesses
+"""
+def main():
+    print "Guess a number between 1 and 100"
+    randomNum = random.randint(1,100)
+
+    found = False
+
+    while not found:
+        userGuess = input("Guess sucker!!! : ")
+
+        if userGuess == randomNum:
+            print "You got it sucka!!! Guess it right"
+            found = True
+        elif userGuess > randomNum:
+            print "Guess Lower"
+        else:
+            print "Guess Higher!!!"
+
+
+if __name__ == "__main__":
+    main()
