4275a8731673d361db3e0b911c8dd653b20396a7
vagrant <vagrant@precise32.(none)>
2015-03-02 09:39:26 -0800

    another variation of fib. This one is more dynamic and doesn't hardcode the 0 and 1 number into the array. The array starts off empty

--- fibonacci.py
+++ fibonacci.py
@@ -7,28 +7,33 @@ E.g fibonacci.py 5 will output 0, 1, 1, 2, 3
 """
 
 import sys
+def getInput():
+    userType = False
+    userInput = input("Enter a number to add up the fib sequence: ")
+    try:
+        int(userInput)
+        userType = True
+    except ValueError:
+        print "Your Input: ", userInput, "is not an integer. Exiting!!"
+        userType = False
 
-def fib(x):
-    fibList = [0, 1]
-    x = int(x) - 2
-    for key in range(x):
-        a = int(fibList[-1])
-        b = int(fibList[-2])
-        x = a + b
-        fibList.append(x)
-    return fibList
-
+    return (userType, userInput)
 
 
 def main():
-    if len(sys.argv) < 2 :
-        print "No arguments given! Exiting! \n"
-        sys.exit(1)
-    elif int(sys.argv[1]) == 0:
-        print "[0]"
-    else:
-        x = int(sys.argv[1])
-        print fib(x)
+    fibArray = []
+    inUser = getInput()
+    if inUser[0] is True:
+        for num in range(inUser[1]):
+            if num == 0 or num == 1:
+                fibArray.append(num)
+            else:
+                newNum = fibArray[-1] + fibArray[-2]
+                fibArray.append(newNum)
+
+        print "FibArray: ", fibArray
+
+        print "Total of all numbers in Array: ", sum(fibArray)
 
 if __name__ == "__main__":
     main()
